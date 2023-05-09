f62d6c19ab6e28da60b6d8de688e5350ea356086
ADVAGA1 <vaqueradriag@gmail.com>
2023-05-09 18:03:47 +0200

    varias plataformas y escalera, de forma procedural

--- My project/Assets/Scripts/CameraMovement.cs
+++ My project/Assets/Scripts/CameraMovement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class CameraMovement : MonoBehaviour
 {
     public float velocity;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         transform.position = transform.position + new Vector3(Time.deltaTime, 0, Time.deltaTime) * velocity;
+        
     }
 }
--- My project/Assets/Scripts/CreateLevel.cs
+++ My project/Assets/Scripts/CreateLevel.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateLevel : MonoBehaviour
 {
-    public GameObject groundPrefab;
+    public GameObject fivePlatforms;
+    public GameObject fourPlatforms;
+    public GameObject threePlatforms;
+    public GameObject sixPlatforms;
+    public GameObject stairs;
     public float scale;
 
     // Start is called before the first frame update
@@ -13,25 +15,56 @@ public class CreateLevel : MonoBehaviour
     {
         GameObject obj;
         bool left = false;
-        uint height = 0;
-        uint width = 0;
+        float height = 0;
+        float width = 0;
+        float depth = 0;
+        int nPlatforms;
 
-        for(uint i = 0; i<15; ++i){
+        List<GameObject> platforms = new List<GameObject>() {threePlatforms,fourPlatforms,fivePlatforms, sixPlatforms};
 
-            obj = (GameObject)Instantiate(groundPrefab);
+        for (uint i = 0; i < 15; ++i)
+        {
+
+            nPlatforms = Random.Range(3, 7);
+
+            obj = Instantiate(platforms[nPlatforms - 3]);
 
             obj.transform.localScale *= scale;
-            obj.transform.Translate(width*scale, 0.0f, height*scale);
+            obj.transform.Translate(width * scale, -depth * scale, height * scale);
             if (left)
             {
-                obj.transform.Translate(3.2f*scale, 0, -3.2f * scale);
+                obj.transform.Translate(3.2f * scale, 0, -3.2f * scale);
                 obj.transform.Rotate(0.0f, 90.0f, 0.0f);
             }
 
             obj.transform.parent = transform;
 
-            if(left) width += 16;
-            else height += 16;
+            if (left) width += 3.2f * nPlatforms;
+            else height += 3.2f * nPlatforms;
+
+            if (i < 14 && Random.value >= 0.5) 
+            {
+                GameObject stair = Instantiate(stairs);
+                
+                stair.transform.localScale *= scale;
+                stair.transform.Translate(width * scale, -depth * scale + 1.6f*scale, height * scale);
+                stair.transform.Rotate(0.0f, 90.0f, 0.0f);
+
+                if (!left)
+                {
+                    stair.transform.Translate(3.2f*scale, 0, 3.2f*scale); 
+                    stair.transform.Rotate(0.0f, 90.0f, 0.0f);
+                }
+
+                stair.transform.parent = transform;
+
+                depth += 1.6f;
+
+                if (left) height += 3.2f;
+                else width += 3.2f;
+
+            }
+
 
             left = !left;
         }
